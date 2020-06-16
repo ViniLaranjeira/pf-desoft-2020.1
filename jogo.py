f6a9d8ff2a5b7773ce8a1611f00d20f04af5993b
Gabriel Paiva De Almeida <56406122+GABRIELPA2@users.noreply.github.com>
2020-06-15 21:09:43 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -85,3 +85,206 @@ class Tile(pygame.sprite.Sprite):
 
         self.rect.x = TILE_SIZE * column
         self.rect.y = TILE_SIZE * row
+# define a classe do jogador
+class Player(pygame.sprite.Sprite):
+    def __init__(self, blocks):
+        pygame.sprite.Sprite.__init__(self)
+        playersheet =    [pygame.image.load(path.join(img_dir, "hero_idle0.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_idle1.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_idle2.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_idle3.png")).convert(),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_idle0.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_idle1.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_idle2.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_idle3.png")).convert(), True, False),
+                          pygame.image.load(path.join(img_dir, "hero_run0.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_run1.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_run2.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_run3.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_run4.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_run5.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_run6.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_run7.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_run8.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_run9.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_run10.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_run11.png")).convert(),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_run0.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_run1.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_run2.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_run3.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_run4.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_run5.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_run6.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_run7.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_run8.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_run9.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_run10.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_run11.png")).convert(), True, False),
+                          pygame.image.load(path.join(img_dir, "hero_attack0.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_attack1.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_attack2.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_attack3.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_attack4.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_attack5.png")).convert(),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_attack0.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_attack1.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_attack2.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_attack3.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_attack4.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_attack5.png")).convert(), True, False),
+                          pygame.image.load(path.join(img_dir, "hero_jump0.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_jump1.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_jump2.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_jump3.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_jump4.png")).convert(),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_jump0.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_jump1.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_jump2.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_jump3.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_jump4.png")).convert(), True, False),
+                          pygame.image.load(path.join(img_dir, "hero_slash0.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_slash1.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_slash2.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_slash3.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_slash4.png")).convert(),
+                          pygame.image.load(path.join(img_dir, "hero_slash5.png")).convert(),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_slash0.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_slash1.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_slash2.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_slash3.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_slash4.png")).convert(), True, False),
+                          pygame.transform.flip(pygame.image.load(path.join(img_dir, "hero_slash5.png")).convert(), True, False)] 
+                          
+        i = 0
+        while i < len(playersheet):
+            if i < 8:
+                playersheet[i] = pygame.transform.scale(playersheet[i],(95,120))
+                self.image = playersheet[i]
+                self.image.set_colorkey(WHITE)
+            elif i < 32:
+                playersheet[i] = pygame.transform.scale(playersheet[i],(165,120))
+                self.image = playersheet[i]
+                self.image.set_colorkey(WHITE)
+            elif i <44:
+                playersheet[i] = pygame.transform.scale(playersheet[i],(240,120))
+                self.image = playersheet[i]
+                self.image.set_colorkey(WHITE)
+            elif i < 54:
+                playersheet[i] = pygame.transform.scale(playersheet[i],(152,192))
+                self.image = playersheet[i]
+                self.image.set_colorkey(WHITE)
+            else:
+                playersheet[i] = pygame.transform.scale(playersheet[i],(222,210))
+                self.image = playersheet[i]
+                self.image.set_colorkey(WHITE)
+            i += 1
+            
+        self.animations = {IDLE:playersheet[0:4],
+                            IDLE_LEFT:playersheet[4:8],
+                            WALK:playersheet[8:20],
+                            WALK_LEFT:playersheet[20:32],
+                            ATTACK:playersheet[32:38],
+                            ATTACK_LEFT:playersheet[38:44],
+                            JUMP:playersheet[44:49],
+                            JUMP_LEFT:playersheet[49:54],
+                            FALL:playersheet[48:49],
+                            FALL_LEFT:playersheet[53:54],
+                            SLASH:playersheet[54:60],
+                            SLASH_LEFT:playersheet[60:66]} 
+        
+        self.state = IDLE
+        self.animation = self.animations[self.state]
+        self.frame = 0
+        self.image = self.animation[self.frame]
+        self.rect = self.image.get_rect()
+        self.fire = False
+        self.blocks = blocks
+        
+        self.rect.x = 0
+        self.rect.bottom = HEIGHT
+        self.speedx = 0
+        self.speedy = 1
+        
+        self.last_update = pygame.time.get_ticks()
+        self.frame_ticks = 100
+        
+    def update(self):
+        
+        now = pygame.time.get_ticks()
+        elapsed_ticks = now - self.last_update
+        if elapsed_ticks > self.frame_ticks:
+            self.last_update = now
+            self.frame += 1
+            self.animation = self.animations[self.state]
+            if self.frame >= len(self.animation):
+                self.frame = 0
+            center = self.rect.center
+            self.image = self.animation[self.frame]
+            self.rect = self.image.get_rect()
+            self.rect.center = center
+            self.mask = pygame.mask.from_surface(self.image)
+    
+        collisions = pygame.sprite.spritecollide(self, self.blocks, False)
+        for collision in collisions:
+            if self.speedy > 0:
+                self.rect.bottom = collision.rect.top
+                if self.state in AIR_R:
+                    self.state = IDLE
+                elif self.state in AIR_L:
+                    self.state = IDLE_LEFT
+                self.speedy = 0
+            elif self.speedy < 0:
+                self.rect.top = collision.rect.bottom
+                if self.state in AIR_R:
+                    self.state = IDLE
+                elif self.state in AIR_L:
+                    self.state = IDLE_LEFT
+                self.speedy = 0   
+            
+        if self.speedy != 0:
+            self.speedy += GRAVITY
+        self.rect.y += self.speedy
+        
+        keys = pygame.key.get_pressed() 
+        if self.state == IDLE or self.state == IDLE_LEFT:  
+            if keys[pygame.K_d] == True and keys[pygame.K_a] == False:
+                self.state = WALK
+            elif keys[pygame.K_a] == True and keys[pygame.K_d] == False:
+                self.state = WALK_LEFT
+        if keys[pygame.K_p] == False and self.frame == 5:
+            if self.state == ATTACK or self.state == SLASH:
+                self.state = IDLE
+            elif self.state == ATTACK_LEFT or self.state == SLASH_LEFT:
+                self.state = IDLE_LEFT
+            self.fire = False
+        if self.fire == True and self.state == IDLE:
+            self.state = ATTACK
+        elif self.fire == True and self.state == IDLE_LEFT:
+            self.state = ATTACK_LEFT
+        elif self.fire == True and self.state == WALK:
+            self.state = SLASH
+        elif self.fire == True and self.state == WALK_LEFT:
+            self.state = SLASH_LEFT
+            
+        if self.speedy < 0 and self.state in POS:
+            self.state = JUMP
+        elif self.speedy > 0 and self.state in POS:
+            self.state = FALL
+        elif self.speedy < 0 and self.state in NEG:
+            self.state = JUMP_LEFT 
+        elif self.speedy > 0 and self.state in NEG:
+            self.state = FALL_LEFT
+            
+        self.rect.x += self.speedx
+        
+    def jump(self):
+        
+        if self.state in POS and self.state not in AIR:
+            self.speedy -= JUMP_SIZE
+            self.state = JUMP
+        elif self.state in POS and self.state not in AIR:
+            self.speedy -= JUMP_SIZE
+            self.state = JUMP_LEFT
+        self.speedy += GRAVITY
+        self.rect.y += self.speedy
